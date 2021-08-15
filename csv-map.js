570f054499518e17d0bfcb3bdbb1734dc2744522
Taisuke Fukuno <fukuno@jig.jp>
2021-08-15 10:34:39 +0900

    support custom map

--- csv-map.js
+++ csv-map.js
@@ -11,28 +11,9 @@ const omit = (s, len) => {
   }
   return s;
 };
-const makeTable = (d) => {
-  const tbl = [];
-  tbl.push("<table>");
-  for (const name in d) {
-    let val = d[name];
-    if (val && (val.startsWith("http://") || val.startsWith("https://"))) {
-      val = "<a href=" + val + ">" + omit(val, 30) + "</a>";
-    }
-    if (val) {
-      if (name == "sabaecc:geo3x3") {
-        tbl.push(`<tr><th>${name}</th><td><a href=https://code4sabae.github.io/geo3x3-map/#${val}>${val}</a></td></tr>`);
-      } else {
-        tbl.push(`<tr><th>${name}</th><td>${val}</td></tr>`);
-      }
-    }
-  }
-  tbl.push("</table>");
-  return tbl.join("");
-};
 
 class CSVMap extends HTMLElement {
-  constructor () {
+  constructor() {
     super();
     this.init();
   }
@@ -85,14 +66,104 @@ class CSVMap extends HTMLElement {
     this.data = d;
     this.redraw();
   }
-  async redraw() {
-    const data = this.data;
-
+  async getLatLng(d) {
+    const geo3x3 = d["sabaecc:geo3x3"] || d["geo3x3"] || d["Geo3x3"];
+    if (geo3x3) {
+      const pos = Geo3x3.decode(geo3x3);
+      if (pos) {
+        return [pos.lat, pos.lng];
+      }
+    }
+    const lat = d["schema:latitude"] || d["latitude"] || d["lat"] || d["緯度"] || d["ic:緯度"];
+    const lng = d["schema:longitude"] || d["longitude"] || d["lng"] || d["lon"] || d["long"] || d["経度"] || d["ic:経度"];
+    if (lat && lng) {
+      return [lat, lng];
+    }
+    const img = d["photo"] || d["image"];
+    if (!img) {
+      return null;
+    }
+    const bin = new Uint8Array(await (await fetch(img)).arrayBuffer());
+    const exif = EXIF.readFromBinaryFile(bin.buffer);
+    if (exif) {
+      const ll = EXIF.toLatLng(exif);
+      if (ll) {
+        return [ll.lat, ll.lng];
+      }
+    }
+    return null;
+  };
+  makeTable(d) {
+    const tbl = [];
+    tbl.push("<table>");
+    for (const name in d) {
+      let val = d[name];
+      if (val && (val.startsWith("http://") || val.startsWith("https://"))) {
+        val = "<a href=" + val + ">" + omit(val, 30) + "</a>";
+      }
+      if (val) {
+        if (name == "sabaecc:geo3x3") {
+          tbl.push(`<tr><th>${name}</th><td><a href=https://code4sabae.github.io/geo3x3-map/#${val}>${val}</a></td></tr>`);
+        } else {
+          tbl.push(`<tr><th>${name}</th><td>${val}</td></tr>`);
+        }
+      }
+    }
+    tbl.push("</table>");
+    return tbl.join("");
+  }
+  async getMarker(d, ll) {
     const icon = this.getAttribute("icon");
     const iconsize = this.getAttribute("iconsize") || 30;
-    const filter = this.getAttribute("filter")?.split(",");
     const allcolor = this.getAttribute("color");
 
+    const title = d["schema:name"] || d["name"];
+    const opt = { title };
+    const icon2 = d["photo"] || d["image"] || icon;
+    const iconsize2 = iconsize * 2;
+    if (icon2) {
+      const img = await fetchImage(icon2);
+      const size = getResized(img.width, img.height, iconsize2);
+      const iconw = size.width;
+      const iconh = size.height;
+      opt.icon = L.icon({
+        iconUrl: icon2,
+        iconRetilaUrl: icon2,
+        iconSize: [iconw, iconh],
+        iconAnchor: [iconw / 2, iconh / 2],
+        popupAnchor: [0, -iconh / 2],
+      });
+    } else {
+      const color = d["color"] || allcolor;
+      if (LeafletSprite.colors.indexOf(color) >= 0) {
+        opt.icon = L.spriteIcon(color);
+      }
+    }
+
+    return L.marker(ll, opt);
+  }
+  async bindPopup(d, marker) {
+    const title = d["schema:name"] || d["name"];
+    const filter = this.getAttribute("filter")?.split(",");
+    const url = d["schema:url"] || d["url"];
+
+    const d2 = (() => {
+      if (!filter) {
+        return d;
+      }
+      const res = {};
+      for (const n in d) {
+        if (filter.indexOf(n) >= 0) {
+          res[n] = d[n];
+        }
+      }
+      return res;
+    })();
+    const tbl = this.makeTable(d2);
+    marker.bindPopup((title ? (url ? `<a href=${url}>${title}</a>` : title) : "") + tbl);
+  }
+
+  async redraw() {
     if (this.iconlayer) {
       this.map.removeLayer(this.iconlayer);
     }
@@ -100,80 +171,16 @@ class CSVMap extends HTMLElement {
     this.iconlayer.addTo(this.map);
 
     const lls = [];
-    for (const d of data) {
-      const getLatLng = async (d) => {
-        const geo3x3 = d["sabaecc:geo3x3"] || d["geo3x3"] || d["Geo3x3"];
-        if (geo3x3) {
-          const pos = Geo3x3.decode(geo3x3);
-          if (pos) {
-            return [pos.lat, pos.lng];
-          }
-        }
-        const lat = d["schema:latitude"] || d["latitude"] || d["lat"] || d["緯度"] || d["ic:緯度"];
-        const lng = d["schema:longitude"] || d["longitude"] || d["lng"] || d["lon"] || d["long"] || d["経度"] || d["ic:経度"];
-        if (lat && lng) {
-          return [lat, lng];
-        }
-        const img = d["photo"] || d["image"];
-        if (!img) {
-          return null;
-        }
-        const bin = new Uint8Array(await (await fetch(img)).arrayBuffer());
-        const exif = EXIF.readFromBinaryFile(bin.buffer);
-        if (exif) {
-          const ll = EXIF.toLatLng(exif);
-          if (ll) {
-            return [ll.lat, ll.lng];
-          }
-        }
-        return null;
-      };
-      const ll = await getLatLng(d);
+    for (const d of this.data) {
+      const ll = await this.getLatLng(d);
       if (!ll) {
         continue;
       }
-      const title = d["schema:name"] || d["name"];
-      const url = d["schema:url"] || d["url"];
-      const opt = { title };
-      const icon2 = d["photo"] || d["image"] || icon;
-      const iconsize2 = iconsize * 2;
-      if (icon2) {
-        const img = await fetchImage(icon2);
-        const size = getResized(img.width, img.height, iconsize2);
-        const iconw = size.width;
-        const iconh = size.height;
-        opt.icon = L.icon({
-          iconUrl: icon2,
-          iconRetilaUrl: icon2,
-          iconSize: [iconw, iconh],
-          iconAnchor: [iconw / 2, iconh / 2],
-          popupAnchor: [0, -iconh / 2],
-        });
-      } else {
-        const color = d["color"] || allcolor;
-        if (LeafletSprite.colors.indexOf(color) >= 0) {
-          opt.icon = L.spriteIcon(color);
-        }
-      }
-
-      const marker = L.marker(ll, opt);
-
-      const d2 = (() => {
-        if (!filter) {
-          return d;
-        }
-        const res = {};
-        for (const n in d) {
-          if (filter.indexOf(n) >= 0) {
-            res[n] = d[n];
-          }
-        }
-        return res;
-      })();
-      const tbl = makeTable(d2);
-      marker.bindPopup((title ? (url ? `<a href=${url}>${title}</a>` : title) : "") + tbl);
-      this.iconlayer.addLayer(marker);
+      const marker = await this.getMarker(d, ll);
+      await this.bindPopup(d, marker);
       lls.push(ll);
+
+      this.iconlayer.addLayer(marker);
     }
     if (lls.length) {
       this.map.fitBounds(lls);
@@ -181,6 +188,6 @@ class CSVMap extends HTMLElement {
   }
 }
 
-customElements.define('csv-map', CSVMap);
+customElements.define("csv-map", CSVMap);
 
 export { CSVMap };
