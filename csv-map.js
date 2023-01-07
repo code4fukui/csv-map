e1c9dd350e50739b9e5dafd015243644f4fff70a
Taisuke Fukuno <fukuno@jig.jp>
2023-01-07 16:09:39 +0900

    support mode=satellite

--- csv-map.js
+++ csv-map.js
@@ -62,7 +62,7 @@ class CSVMap extends HTMLElement {
     // set 国土地理院地図 https://maps.gsi.go.jp/development/ichiran.html
     const land = "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png";
     const sat = "https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg";
-    L.tileLayer(this.getAttribute("satellite") ? sat : land, {
+    L.tileLayer(this.getAttribute("mode") == "satellite" ? sat : land, {
       attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>"',
       maxZoom: 18,
     }).addTo(this.map);
